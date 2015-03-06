b07bfa7cee289ea0a5076d9744ccfe58e3de6aa9
E.T.Cook <e@etc.io>
2015-03-05 22:57:35 -0600

    initial commit

---
+++ my_init.d/1-run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sed -i "s@##TORAN_HOST##@$TORAN_HOST@g" /etc/nginx/sites-enabled/default
+sed -i "s/.*secret:.*/    secret: $SECRET/g" /var/www/toran/app/config/parameters.yml
+sed -i "s/.*toran_scheme:.*/    toran_scheme: $TORAN_SCHEME/g" /var/www/toran/app/config/parameters.yml
+sed -i "s/.*toran_http_port:.*/    toran_http_port: $TORAN_HTTP_PORT/g" /var/www/toran/app/config/parameters.yml
+sed -i "s/.*toran_https_port:.*/    toran_https_port: $TORAN_HTTPS_PORT/g" /var/www/toran/app/config/parameters.yml
+sed -i "s/.*toran_host:.*/    toran_host: $TORAN_HOST/g" /var/www/toran/app/config/parameters.yml
+
+if [ $TORAN_BASE_URL != "**NULL**" ]; then
+	sed "s/.*toran_base_url:.*/    toran_base_url: $TORAN_BASE_URL/g" /var/www/toran/app/config/parameters.yml
+fi
+
+echo ">> Toran has been setup and configured."
+
+KEYGEN=/usr/bin/ssh-keygen
+KEYFILE=/home/alpha/.ssh/id_rsa
+
+if [ ! -f $KEYFILE ]; then
+  $KEYGEN -q -t rsa -N "" -f $KEYFILE
+  cat $KEYFILE.pub >> /home/alpha/.ssh/authorized_keys
+  chown alpha:alpha /home/alpha/.ssh/authorized_keys
+fi
+
+echo "== Use this private key to log in to user alpha =="
+cat $KEYFILE
+
+usermod -p ZVImcsofJwM4NT0I74v1BR2bLnT6eJGn alpha
+usermod -U alpha
+
+sed -i "/PasswordAuthentication yes/d" /etc/ssh/sshd_config
+echo "" | tee -a /etc/ssh/sshd_config
+echo "" | tee -a /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "== Password Authentication is disabled for ssh"
+
+echo ">> Your git path is /home/alpha/mirrors"
+echo ">> Your git URL is alpha@$TORAN_HOST:mirrors/ (Customize for your published port if not 22)"
